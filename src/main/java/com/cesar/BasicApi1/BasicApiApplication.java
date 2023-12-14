c3930eceec233c290cb514eb33010acf5126a3c1
cesar <cesarpazol1029@gmail.com>
2023-12-13 19:36:14 -0600

    Prometheus config. Grafana connection

--- src/main/java/com/cesar/BasicApi1/BasicApiApplication.java
+++ src/main/java/com/cesar/BasicApi1/BasicApiApplication.java
@@ -1,4 +1,4 @@
-package com.cesar.BasicApi;
+package com.cesar.BasicApi1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/cesar/BasicApi1/controller/Controller.java
+++ src/main/java/com/cesar/BasicApi1/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.cesar.BasicApi.controller;
+package com.cesar.BasicApi1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
